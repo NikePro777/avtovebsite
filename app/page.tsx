0c61173b3c2783a37570c73148cf0ce3dcc68bd5
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-10-16 22:40:33 +0500

    add animation balls

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import styles from '../app/styles/Home.module.css';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div className={styles.container}>
+      <div className="background-animation"></div>
+      <div id="ball-one"></div>
+      <div id="ball-two"></div>
+      <div id="ball-three"></div>
+    </div>
+  );
 }
